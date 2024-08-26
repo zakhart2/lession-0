c45b8ff242b6f8fddece7ebdd576522bc362b1e9
zakhart2 <zakhar.tkach1@mail.ru>
2024-08-26 14:51:20 +0300

    Add files via upload

---
+++ lession0.py
@@ -0,0 +1,8 @@
+# 1st program
+print(9 ** 0.5 * 5)
+# 2nd program
+print(9.99 > 9.98, 1000 != 1000.1)
+# 3nd program
+print(2 * 2 + 2 , 2 * (2 + 2),2 * 2 + 2 == 2 * (2 + 2) )
+# 4th program
+print(int((float('123.456')-int(float('123.456')))*10))
